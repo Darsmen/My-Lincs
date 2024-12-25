04f2974cf5d173c72b66a57fac0f87e9688638bd
Darsmen <96358986+Darsmen@users.noreply.github.com>
2024-12-25 21:09:54 +0300

    add lincs

--- javascript.js
+++ javascript.js
@@ -2,3 +2,11 @@ function tgbut()
 {
     window.open('https://t.me/Darsmen');
 }
+function stbut()
+{
+    window.open('https://steamcommunity.com/id/Darsmen');
+}
+function dss1but()
+{
+    window.open('https://discord.gg/DnvHha76Zv');
+}
